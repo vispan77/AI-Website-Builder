1c00ceb8ad62331b57fbf173fe909c863f363bf3
vispan77 <vishlpandey2332@gmail.com>
2026-05-14 14:25:32 +0530

    implemented generate website in frontend

--- client/src/pages/Generate.jsx
+++ client/src/pages/Generate.jsx
@@ -1,12 +1,78 @@
+import { ArrowLeft } from 'lucide-react';
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { motion } from "motion/react"
 
 function Generate() {
-  return (
-    <div className='min-h-screen bg-[#050505] text-white'>
-        
-      
-    </div>
-  )
+    const navigate = useNavigate();
+    return (
+        <div className='min-h-screen bg-linear-to-br from-[#050505] 
+           via-[#0b0b0b] to-[#050505] text-white'>
+            <div className='sticky top-0 z-50 backdrop-blur-xl bg-black/50 border-b border-white/10'>
+                <div className='max-w-7xl mx-auto px-6 h-16 flex items-center justify-between'>
+                    <div className='flex items-center gap-4'>
+                        <button onClick={() => navigate("/dashboard")}
+                            className='p-2 rounded-lg hover:bg-white/10 transition'
+                        >
+                            <ArrowLeft size={16} />
+                        </button>
+                        <h1 className='text-lg font-semibold'>
+                            Genweb<span className='text-zinc-400'>.ai</span>
+                        </h1>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className='max-w-6xl mx-auto px-6 py-16'>
+                <motion.div
+                    initial={{ opacity: 0, y: 30 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    className='mb-16 text-center'
+                >
+                    <h1 className='text-4xl md:text-5xl mb-5 font-bold leading-tight'>
+                        Build Website with
+                        <span className='block bg-linear-to-r from-white to-zinc-400
+                        bg-clip-text text-transparent'>
+                            Real AI Power
+                        </span>
+                    </h1>
+                    <p className='text-zinc-400 max-w-2xl mx-auto'>
+                        This process may take serveral minutes.
+                        Genweb.ai focuses on quality, not shortcuts.
+                    </p>
+                </motion.div>
+
+                <div className='mb-14'>
+                    <h1 className='text-xl font-semibold mb-5 ml-3'>Describe your website</h1>
+                    <div className='relative'>
+                        <textarea name="" id=""
+                            placeholder='Deacribe your website in details...'
+                            className='w-full h-56 p-6 rounded-3xl bg-black/60
+                            border border-white/10 outline-none resize-none text-sm leading-relaxed
+                            focus:ring-2 focus:ring-white/20 transition'
+                        />
+                    </div>
+                </div>
+
+                <div className='flex text-center justify-center'>
+                    <motion.button
+                    initial={{opacity:0, y:50}}
+                    whileInView={{opacity:1, y:0}}
+                    whileHover={{scale: 1.05}}
+                    whileTap={{scale: 0.95}}
+                    className='px-14 py-4 rounded-2xl text-lg font-semibold bg-white text-black'
+                    >
+                        Generate Website
+                    </motion.button>
+
+                </div>
+
+            </div>
+
+
+        </div>
+    )
 }
 
 export default Generate
